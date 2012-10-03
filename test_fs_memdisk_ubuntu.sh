9a26e584221e47772952e846125906ff72877acc
Charlie Root <azovmash@ukr.net>
2012-10-03 04:48:41 +0300

    Добавлены xfs jfs zfs для Ubuntu.

--- test_fs_memdisk_ubuntu.sh
+++ test_fs_memdisk_ubuntu.sh
@@ -1,75 +1,109 @@
 #!/bin/sh
 
 # Создаем диск в памяти на 6 гиг (всего 8) итого системе остаётся 2гига.
-DISKDEV="md0"
-DISKSIZE="6G"
+# echo 'GRUB_CMDLINE_LINUX="ramdisk_size=6291456"' >/etc/default/grub
 TESTSIZE="-s 5700m:128k -r 1800m"
 TEST2SIZE="bs=1M count=5400"
-UFS="-O2"
 TOLOG="2>>&./test_01_memdisk_bsd.sh"
-
-start_md ()
-{
-/bin/echo "Starting memdisk "
-/bin/mkdir -p /mnt/ramdisk
-/bin/mkdir -p /mnt/fs
-/bin/mount -t ramfs -omasxsize=$DISKSIZE ramdisk /mnt/ramdisk
-/bin/dd if=/dev/zero of=/mnt/ramdisk/fs bs=1M count=6144
-/bin/echo
-}
-
-stop_md ()
-{
-/bin/echo "Shutting memdisk "
-/bin/umount -f /mnt/ramdisk
-/bin/echo
-}
 
 new_fs_ext4 ()
 {
 /bin/echo "Starting newfs ext4 "
-/sbin/mkfs.ext4 /mnt/ramdisk/fs <<EOF
+/sbin/mkfs.ext4 /dev/ram0 <<EOF
 y
 EOF
-/bin/mount -o loop /mnt/ramdisk/fs /mnt/fs
-/bin/mount
-/bin/df -H
+/bin/mount /dev/ram0 /mnt/fs
+/bin/mount |grep ram
+/bin/df -H |grep ram
 /bin/echo
 }
 
 new_fs_ext3 ()
 {
 /bin/echo "Starting newfs ext3 "
-/sbin/mkfs.ext3 /mnt/ramdisk/fs <<EOF
+/sbin/mkfs.ext3 /dev/ram0 <<EOF
 y
 EOF
-/bin/mount -o loop /mnt/ramdisk/fs /mnt/fs
-/bin/mount
-/bin/df -H
+/bin/mount /dev/ram0 /mnt/fs
+/bin/mount |grep ram
+/bin/df -H |grep ram
 /bin/echo
 }
 
 new_fs_ext2 ()
 {
 /bin/echo "Starting newfs ext2 "
-/sbin/mkfs.ext2 /mnt/ramdisk/fs <<EOF
+/sbin/mkfs.ext2 /dev/ram0 <<EOF
 y
 EOF
-/bin/mount -o loop /mnt/ramdisk/fs /mnt/fs
-/bin/mount
-/bin/df -H
+/bin/mount /dev/ram0 /mnt/fs
+/bin/mount |grep ram
+/bin/df -H |grep ram
 /bin/echo
 }
 
 new_fs_btrfs ()
 {
 /bin/echo "Starting newfs btrfs "
-/sbin/mkfs.btrfs /mnt/ramdisk/fs <<EOF
+/sbin/mkfs.btrfs /dev/ram0 <<EOF
 y
 EOF
-/bin/mount -o loop /mnt/ramdisk/fs /mnt/fs
-/bin/mount
-/bin/df -H
+/bin/mount /dev/ram0 /mnt/fs
+/bin/mount |grep ram
+/bin/df -H |grep ram
+/bin/echo
+}
+
+new_fs_xfs ()
+{
+/bin/echo "Starting newfs xfs "
+/sbin/mkfs.xfs -f /dev/ram0 <<EOF
+y
+EOF
+/bin/mount /dev/ram0 /mnt/fs
+/bin/mount |grep ram
+/bin/df -H |grep ram
+/bin/echo
+}
+
+new_fs_jfs ()
+{
+/bin/echo "Starting newfs jfs "
+/sbin/mkfs.jfs /dev/ram0 <<EOF
+y
+EOF
+/bin/mount /dev/ram0 /mnt/fs
+/bin/mount |grep ram
+/bin/df -H |grep ram
+/bin/echo
+}
+
+new_fs_zfs ()
+{
+/bin/echo "Starting newfs ZFS "
+/sbin/zpool create mdpool /dev/ram0
+/sbin/zpool list
+/sbin/zpool status
+/sbin/zfs set mountpoint=/mnt/fs mdpool
+#/sbin/zfs get recordsize
+#/sbin/zdb -U /var/tmp/zpool.cache |grep ashift
+/bin/mount |grep mdpool
+/bin/df -H |grep mdpool
+/bin/echo
+}
+
+new_fs_zfs_checksumoff ()
+{
+/bin/echo "Starting newfs ZFS checksum=off"
+/sbin/zpool create mdpool /dev/ram0
+/sbin/zpool list
+/sbin/zpool status
+/sbin/zfs set checksum=off mdpool
+/sbin/zfs set mountpoint=/mnt/fs mdpool
+#/sbin/zfs get recordsize
+#/sbin/zdb -U /var/tmp/zpool.cache |grep ashift
+/bin/mount |grep mdpool
+/bin/df -H |grep mdpool
 /bin/echo
 }
 
@@ -101,6 +135,38 @@ umount -f /mnt/fs
 /bin/echo
 }
 
+stop_fs_xfs ()
+{
+/bin/echo "Stoping newfs xfs "
+umount -f /mnt/fs
+/bin/echo
+}
+
+stop_fs_jfs ()
+{
+/bin/echo "Stoping newfs jfs "
+umount -f /mnt/fs
+/bin/echo
+}
+
+stop_fs_zfs ()
+{
+/bin/echo "Stoping newfs zfs "
+umount -f /mnt/fs
+sleep 5
+/sbin/zpool destroy mdpool
+/bin/echo
+}
+
+stop_fs_zfs_checksumoff ()
+{
+/bin/echo "Stoping newfs zfs checksum=off "
+umount -f /mnt/fs
+sleep 5
+/sbin/zpool destroy mdpool
+/bin/echo
+}
+
 test_bonnie ()
 {
 /bin/echo "Starting test bonnie  $TESTSIZE $TESTNAME"
@@ -127,8 +193,7 @@ test_dd ()
 /bin/echo
 }
 
-
-start_md
+#start
 
 new_fs_ext4
 TESTNAME="-m ext4"
@@ -148,14 +213,37 @@ test_bonnie
 #test_dd
 stop_fs_ext2
 
-# Мы не будем ее тестировать потому что она страдает детскими болезнями.
-#new_fs_btrfs
-#TESTNAME="-m btrfs"
-#test_bonnie
+new_fs_xfs
+TESTNAME="-m xfs"
+test_bonnie
 #test_dd
-#stop_fs_btrfs
+stop_fs_xfs
 
-stop_md
+new_fs_jfs
+TESTNAME="-m jfs"
+test_bonnie
+#test_dd
+stop_fs_jfs
+
+new_fs_zfs
+TESTNAME="-m zfs"
+test_bonnie
+#test_dd
+stop_fs_zfs
+
+new_fs_zfs_checksumoff
+TESTNAME="-m zfs_checksumoff"
+test_bonnie
+#test_dd
+stop_fs_zfs_checksumoff
+
+new_fs_btrfs
+TESTNAME="-m btrfs"
+test_bonnie
+#test_dd
+stop_fs_btrfs
+
+#stop
 
 
 
